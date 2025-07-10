3c1b050044270012000c02a09bc8679054da27f7
Aminy Ghaisan <69082746+aminyG@users.noreply.github.com>
2025-07-10 11:28:09 +0700

    linked-forgot-password-api

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Http;
 
 class PasswordResetLinkController extends Controller
 {
@@ -23,22 +25,30 @@ class PasswordResetLinkController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'email' => ['required', 'email'],
+public function store(Request $request): RedirectResponse
+{
+    $request->validate([
+        'email' => ['required', 'email'],
+    ]);
+
+    $response = Http::withHeaders([
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+    ])->post('http://presence.guestallow.com/api/auth/forgot-password', [
+        'email' => $request->email,
+    ]);
+
+    if ($response->successful()) {
+        return back()->with('status', $response->json('message'));
+    } elseif ($response->status() == 422) {
+        return back()->withInput()->withErrors([
+            'email' => 'Email tidak valid atau tidak ditemukan.',
         ]);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-
-        return $status == Password::RESET_LINK_SENT
-            ? back()->with('status', __($status))
-            : back()->withInput($request->only('email'))
-                ->withErrors(['email' => __($status)]);
+    } else {
+        return back()->withInput()->withErrors([
+            'email' => 'Terjadi kesalahan saat mengirim link reset password.',
+        ]);
     }
+}
+
 }
